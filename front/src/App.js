f9e774294e2dab8a03c95e6b04db3c2a5203e428
ahmed Saadi <ahmed@ahmeds-Laptop.local>
2023-08-15 16:31:25 +0200

    setup project front

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import './styles/App.css';
+import AstronautList from './components/AstronautList';
+import AstronautForm from './components/AstronautForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ <AstronautList/>}/>
+        <Route path="/add" element={<AstronautForm/>} />
+        <Route path="/edit/:id" element={<AstronautForm/>} />
+      </Routes>
+    </Router>
   );
 }
 
